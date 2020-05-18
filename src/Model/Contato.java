3899251f837daf7f18c81235a16a63b062a04762
Manoel Carvalho <manoelmecat@gmail.com>
2020-05-18 13:18:55 -0300

    Views

---
+++ src/Model/Contato.java
@@ -0,0 +1,70 @@
+package Model;
+
+public class Contato {
+       private int id;
+       private String nome;
+       private String email;
+       private String telefone;       
+       private String celular;
+       private String foto;
+       private String nasc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getNasc() {
+        return nasc;
+    }
+
+    public void setNasc(String nasc) {
+        this.nasc = nasc;
+    }
+       
+       
+       
+}
